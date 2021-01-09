7c8306fc9b3fc03bbc21ca5eb995da9550a20806
CJ <chiji.davids@yahoo.com>
2021-01-09 14:47:58 -0700

    Styling filter buttons and updating functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 // CSS
 import "./App.css";
 // Components
@@ -11,11 +11,31 @@ function App() {
   const [jobs, setJobs] = useState(dataJs);
   const [filters, setFilters] = useState([]);
 
+  const checkFilter = () => {
+    let filterLength;
+    const jobContainer = document.querySelector("main");
+    const filterContainer = document.querySelector(".filter-ctn");
+    const filterWrapper = document.querySelector(".filter-wrapper");
+    filterLength = filters.length;
+
+    if (filterLength === 0) {
+      filterContainer.classList.add("filter-ctn-display");
+      jobContainer.classList.add("main-display");
+    } else {
+      filterContainer.classList.remove("filter-ctn-display");
+      jobContainer.classList.remove("main-display");
+      filterWrapper.classList.add("filter-wrapper-display");
+    }
+  };
+
+  useEffect(() => {
+    checkFilter();
+  }, [filters]);
+
   return (
     <div>
-      <header>
-        <FilterList filters={filters} setFilters={setFilters} />
-      </header>
+      <header></header>
+      <FilterList filters={filters} setFilters={setFilters} />
       <JobList jobs={jobs} filters={filters} setFilters={setFilters} />
       <footer>
         <Attribution />
--- src/components/FilterList.js
+++ src/components/FilterList.js
@@ -1,17 +1,34 @@
 import React from "react";
-import { RiCloseLine } from "react-icons/ri";
+import removeImg from "../images/icon-remove.svg";
 
 function FilterList({ filters, setFilters }) {
+  const handleClick = (e) => {
+    let closeBtn, buttonName, newFilterList;
+    closeBtn = e.target;
+    buttonName = closeBtn.parentNode.parentNode.firstChild.textContent;
+
+    newFilterList = filters.filter((filterItem) => filterItem !== buttonName);
+
+    setFilters(newFilterList);
+  };
+
   return (
-    <div>
-      {filters.map((eachFilter, index) => (
-        <div key={index} className="filter">
-          <div className="filter-name">{eachFilter}</div>
-          <button className="close-btn">
-            <RiCloseLine />
-          </button>
-        </div>
-      ))}
+    <div className="filter-ctn">
+      <div className="filter-wrapper">
+        {filters.map((eachFilter, index) => (
+          <span id={index} key={index} className="filter">
+            <button className="filter-name">{eachFilter}</button>
+            <button className="close-btn" type="button" name="close-btn">
+              <img
+                onClick={handleClick}
+                className="close-icon"
+                src={removeImg}
+                alt="remove-icon"
+              />
+            </button>
+          </span>
+        ))}
+      </div>
     </div>
   );
 }
